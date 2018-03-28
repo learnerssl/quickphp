1ad8913eb19612893c1848f3e535ba041d25f03d
shensilu <312785362@qq.com>
2018-03-28 10:40:17 +0800

    commit

--- application/api/api_base.php
+++ application/api/api_base.php
@@ -2,72 +2,35 @@
 
 namespace application\api;
 
+use quickphp\lib\Response;
+
 class api_base
 {
-    public $allow_headers = [
-        'Origin',
-        'No-Cache',
-        'X-Requested-With',
-        'If-Modified-Since',
-        'Pragma',
-        'Last-Modified',
-        'Cache-Control',
-        'Expires',
-        'Content-Type',
-        'X-E4M-With',
-        'Api-Token',
-    ];
-
-    public $allow_origins = [
-        '',
-    ];
-
     function __construct()
     {
     }
 
+    /**
+     * 检查并输出错误信息
+     * @param $info
+     */
+    function check_error($info)
+    {
+        if (self::is_error($info)) {
+            Response::api_response($info['error'], $info['etext']);
+        }
+    }
 
     /**
-     * 输出
-     * @param array|string $data 数据
-     * @param int|string $error 错误代码|错误描述
+     * 检查是否存在错误
+     * @param mixed $info
+     * @return bool
      */
-    function output($data, $error = null)
+    public static function is_error($info)
     {
-        //检查错误
-        $ecode = 0;
-        $etext = null;
-        foreach (\data::$error_codes as $key => $item) {
-            if ($item['id'] == $error) {
-                $ecode = $item['id'];
-                $etext = $item['text'];
-                break;
-            }
+        if (is_array($info) && !empty($info['etext'])) {
+            return true;
         }
-        if ($ecode == 0 && $error !== null) {
-            $ecode = -1;
-            $etext = $error;
-        }
-
-        //组装数据
-        $result = [
-            'data' => $data,
-            'error' => $ecode,
-            'etext' => $etext
-        ];
-        $result = json_encode($result);
-
-        //通行header
-        header('Access-Control-Allow-Headers: ' . join(',', $this->allow_headers));
-
-        //允许跨域请求
-        $origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
-        if (in_array($origin, $this->allow_origins)) {
-            header('Access-Control-Max-Age:' . 300);
-            header('Access-Control-Allow-Origin:' . $origin);
-        }
-
-        //输出
-        die($result);
+        return false;
     }
 }
--- quickphp/lib/Response.class.php
+++ quickphp/lib/Response.class.php
@@ -12,80 +12,109 @@
 
 namespace quickphp\lib;
 class Response {
-	const FORMAT = 'json';
-	
-	/**
-	 * @desc 按综合方式输出通信数据
-	 * @param  int    $error 状态码  1:请求成功,其余都为请求失败,默认为请求成功
-	 * @param  string $etext 提示信息
-	 * @param  mixed  $data 数据
-	 * @param  string $format 输出格式
-	 * @return bool
-	 */
-	public static function api_response( $error = 1, $etext = "", $data = array(), $format = self::FORMAT )
-	{
-		if ( ! is_numeric( $error ) ) {
-			$error = - 1;
-			$etext = "无效的状态码";
-		}
-		$format = isset( $_GET['format'] ) ? $_GET['format'] : $format;
-		if ( $format === 'json' ) {
-			self::jsonEncode( $error, $etext, $data );
-		} elseif ( $format === 'xml' ) {
-			self::xmlEncode( $error, $etext, $data );
-		}
-		return true;
-	}
-	
-	/**
-	 * @desc 按json方式输出通信数据
-	 * @param  int    $status 状态码  1:请求成功 0:请求失败
-	 * @param  string $msg 提示信息
-	 * @param  array  $data 数据
-	 * @return string
-	 */
-	private static function jsonEncode( $status, $msg, $data )
-	{
-		$json_data = array( 'error' => $status, "etext" => $msg, "data" => $data );
-		exit( json_encode( $json_data, JSON_UNESCAPED_UNICODE ) );
-	}
-	
-	/**
-	 * @desc 按xml方式输出通信数据
-	 * @param  int    $status 状态码  1:请求成功 0:请求失败
-	 * @param  string $msg 提示信息
-	 * @param  array  $data 数据
-	 * @return string
-	 */
-	private static function xmlEncode( $status, $msg, $data )
-	{
-		$xml_data = array( 'error' => $status, "etext" => $msg, "data" => $data );
-		header( "Content-Type:text/xml" );
-		$xml = "<?xml version='1.0' encoding='UTF-8'?>";
-		$xml .= "<root>";
-		$xml .= self::xmlToEncode( $xml_data );
-		$xml .= "</root>";
-		return $xml;
-	}
-	
-	/**
-	 * 将数据转化为xml格式
-	 * @param $data
-	 * @return string
-	 */
-	private static function xmlToEncode( $data )
-	{
-		$xml = $attr = "";
-		foreach ( $data as $key => $value ) {
-			//xml格式节点不能为数字
-			if ( is_numeric( $key ) ) {
-				$attr = " id='{$key}'";
-				$key = "item";
-			}
-			$xml .= "<{$key}{$attr}>";
-			$xml .= is_array( $value ) ? self::xmlToEncode( $value ) : $value;
-			$xml .= "</{$key}>";
-		}
-		return $xml;
-	}
+    const FORMAT = 'json';
+
+    public static $allow_headers = [
+        'Origin',
+        'No-Cache',
+        'X-Requested-With',
+        'If-Modified-Since',
+        'Pragma',
+        'Last-Modified',
+        'Cache-Control',
+        'Expires',
+        'Content-Type',
+        'X-E4M-With',
+        'Api-Token',
+    ];
+
+    public static $allow_origins = [
+        'http://localhost:3000',
+        'http://newsapi.romantic.ren',
+    ];
+
+    /**
+     * @desc 按综合方式输出通信数据
+     * @param  int    $error 状态码  1:请求成功,其余都为请求失败,默认为请求成功
+     * @param  string $etext 提示信息
+     * @param  mixed  $data 数据
+     * @param  string $format 输出格式
+     * @return bool
+     */
+    public static function api_response( $error = 1, $etext = "", $data = array(), $format = self::FORMAT )
+    {
+        //通行header
+        header('Access-Control-Allow-Headers: ' . join(',', self::$allow_headers));
+
+        //允许跨域请求
+        $origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
+        if (in_array($origin, self::$allow_origins)) {
+            header('Access-Control-Max-Age:' . 300);
+            header('Access-Control-Allow-Origin:' . $origin);
+        }
+
+        if ( ! is_numeric( $error ) ) {
+            $error = - 1;
+            $etext = "无效的状态码";
+        }
+        $format = isset( $_GET['format'] ) ? $_GET['format'] : $format;
+        if ( $format === 'json' ) {
+            self::jsonEncode( $error, $etext, $data );
+        } elseif ( $format === 'xml' ) {
+            self::xmlEncode( $error, $etext, $data );
+        }
+        return true;
+    }
+
+    /**
+     * @desc 按json方式输出通信数据
+     * @param  int    $status 状态码  1:请求成功 0:请求失败
+     * @param  string $msg 提示信息
+     * @param  array  $data 数据
+     * @return string
+     */
+    private static function jsonEncode( $status, $msg, $data )
+    {
+        $json_data = array( 'error' => $status, "etext" => $msg, "data" => $data );
+        exit( json_encode( $json_data, JSON_UNESCAPED_UNICODE ) );
+    }
+
+    /**
+     * @desc 按xml方式输出通信数据
+     * @param  int    $status 状态码  1:请求成功 0:请求失败
+     * @param  string $msg 提示信息
+     * @param  array  $data 数据
+     * @return string
+     */
+    private static function xmlEncode( $status, $msg, $data )
+    {
+        $xml_data = array( 'error' => $status, "etext" => $msg, "data" => $data );
+        header( "Content-Type:text/xml" );
+        $xml = "<?xml version='1.0' encoding='UTF-8'?>";
+        $xml .= "<root>";
+        $xml .= self::xmlToEncode( $xml_data );
+        $xml .= "</root>";
+        return $xml;
+    }
+
+    /**
+     * 将数据转化为xml格式
+     * @param $data
+     * @return string
+     */
+    private static function xmlToEncode( $data )
+    {
+        $xml = $attr = "";
+        foreach ( $data as $key => $value ) {
+            //xml格式节点不能为数字
+            if ( is_numeric( $key ) ) {
+                $attr = " id='{$key}'";
+                $key = "item";
+            }
+            $xml .= "<{$key}{$attr}>";
+            $xml .= is_array( $value ) ? self::xmlToEncode( $value ) : $value;
+            $xml .= "</{$key}>";
+        }
+        return $xml;
+    }
 }
